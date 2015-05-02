2d635f59cb86e55c87b63a86c63d607d8a88f56c
ricardolopes86 <ricardolopes18@yahoo.com.br>
2015-05-02 19:26:34 -0300

    Corrigido a passagem de parâmetro para a JSP (view).

--- src/main/java/br/com/buscador/controller/BuscaController.java
+++ src/main/java/br/com/buscador/controller/BuscaController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.buscador.model.Produto;
 
@@ -14,11 +15,14 @@ public class BuscaController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value="/resultado", method=RequestMethod.POST)
-	public String realizaBusca(Produto produto){
+	public ModelAndView realizaBusca(Produto produto){
 		logger.info("Chegou na função de busca\n");
 		Produto produto1 = new Produto();
 		produto1.setNome(produto.getNome());
 		System.out.println("Nome do produto buscado: "+produto1.getNome());
-		return "resultado";
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("produto", produto1);
+		return mv;
 	}
 }
--- src/main/webapp/WEB-INF/views/resultado.jsp
+++ src/main/webapp/WEB-INF/views/resultado.jsp
@@ -1,6 +1,7 @@
 <%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <html lang="pt_BR">
 <head>
 <meta charset="utf-8">
@@ -19,6 +20,14 @@
 		<div class="container">
 			<h1>Resultado:</h1>
 		</div>
+		<table>
+			<tr>
+				<th>Produto</th>
+			</tr>
+			<tr>
+				<td>${produto.nome}</td>
+			</tr>
+		</table>
 		<script src="<c:url value="/resources/js/jquery.min.js" />"></script>
 		<script src="<c:url value="/resources/js/bootstrap.min.js" />"></script>
 	</div>
